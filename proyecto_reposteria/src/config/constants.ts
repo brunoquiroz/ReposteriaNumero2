dd6bbfb1cb32eb03d5435348f07f11ba59a20b60
brunoquiroz <colobrunmon@gmail.com>
2025-08-25 06:05:09 -0400

    sinURL

--- proyecto_reposteria/src/config/constants.ts
+++ proyecto_reposteria/src/config/constants.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://brunoquirozm.com/api';
-export const API_URL = import.meta.env.VITE_API_URL || 'https://brunoquirozm.com';
+export const API_BASE_URL = '/api';
+export const API_URL = '';
